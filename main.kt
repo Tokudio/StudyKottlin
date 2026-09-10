fe5d211545e97903b37cda034c6643a4e4c542ea
Tran Van Manh <manhbeo357159@gmail.com>
2026-09-10 15:15:56 +0700

    Update main.kt

--- main.kt
+++ main.kt
@@ -3,7 +3,7 @@ fun main() {
     year();
 }
 fun year() {
-    val arrays = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    val arrays = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 11,12);
     for (x in arrays) {
         println(x);
     }
